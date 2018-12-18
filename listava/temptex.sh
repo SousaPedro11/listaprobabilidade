7741917ea5596dc3a79547c8270143d2c07bd351
Pedro <ppls2106@gmail.com>
2018-12-18 09:08:08 -0300

    Separação VA e substitutiva

---
+++ listava/temptex.sh
@@ -0,0 +1,3 @@
+echo "Apagando arquivos temporarios do projeto tex."
+sleep 1
+rm -vf ./*.aux ./*.toc ./*.loc ./*.idx ./*.log ./*.lof ./*.out ./*.backup ./*~ ./*.x ./*.m ./*.dat ./*.synctex*
--- trabalho1.r
+++
@@ -1,4 +0,0 @@
-x=2
-y=2
-x+y
-x=rnorm(y)
